31ee0584a277c8658a65ad4b4b83623dcfb35d67
MhmdShamii <mhmddshamii@gmail.com>
2026-08-03 08:09:15 +0300

    Add loss_MSE function and update main.py to calculate Mean Squared Error

--- helpers.py
+++ helpers.py
@@ -2,3 +2,6 @@ import math
 
 def sigmoid(neuVal):
     return 1 / (1 + math.exp(-neuVal)) 
+
+def loss_MSE(y_true, y_pred):
+    return (y_true - y_pred) ** 2
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from Network import Network
+from helpers import loss_MSE
+input_data = [1, 0]
+target_output = [0]
+
 
 net = Network(2, [4, 1])
-print(net.forward_propagation([1, 0]))
+output = net.forward_propagation(input_data)
+print("Output of the network:", output)
+print("Mean Squared Error:", loss_MSE(target_output[0], output[0]))
