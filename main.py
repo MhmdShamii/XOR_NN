3c91c0ca3bf0206560208d707fd7f4c576e53676
MhmdShamii <mhmddshamii@gmail.com>
2026-09-04 03:58:06 +0300

    Add .gitignore and update main.py for input data and blame calculation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Network import Network
-from helpers import loss_MSE
-input_data = [1, 0]
-target_output = [0]
+from helpers import loss_MSE,blame
+input_data = [[1, 1],[1, 0],[0, 1],[0, 0]]
+target_output = [1, 0, 0, 1]
 
 
 net = Network(2, [4, 1])
 output = net.forward_propagation(input_data)
 print("Output of the network:", output)
 print("Mean Squared Error:", loss_MSE(target_output[0], output[0]))
+print("Blame:", blame(output[0], target_output[0]))
