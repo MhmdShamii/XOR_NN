7adac183425978011773ae6635d9d85e55b40b16
MhmdShamii <mhmddshamii@gmail.com>
2026-09-04 04:02:13 +0300

    Fix .gitignore entry and update main.py for improved readability; adjust helpers.py for consistent formatting

--- helpers.py
+++ helpers.py
@@ -5,3 +5,6 @@ def sigmoid(neuVal):
 
 def loss_MSE(y_true, y_pred):
     return (y_true - y_pred) ** 2
+
+def blame(y_pred, y_true):
+    return 2 * (y_pred - y_true)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Network import Network
 from helpers import loss_MSE,blame
+
 input_data = [[1, 1],[1, 0],[0, 1],[0, 0]]
 target_output = [1, 0, 0, 1]
 
