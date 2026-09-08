dc6010b86b0d6a3a36fd12509e54768834b0562d
mhmdshamii <mhmddshamii@gmail.com>
2026-09-08 03:36:33 +0300

    testing 101

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ target_output = [1, 0, 0, 1]
 
 
 net = Network(2, [4, 1])
-output = net.forward_propagation(input_data)
+output = net.forward_propagation(input_data[0])
 print("Output of the network:", output)
 print("Mean Squared Error:", loss_MSE(target_output[0], output[0]))
 print("Blame:", blame(output[0], target_output[0]))
